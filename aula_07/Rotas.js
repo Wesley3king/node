74433b3d5d6bfdd65d425bce9df6224ca5fc214e
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-05-06 10:57:12 -0300

    Update Rotas.js

--- aula_07/Rotas.js
+++ aula_07/Rotas.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const url = require('url');
 const rotas = express.Router();
 
 const cursos = [
@@ -10,10 +11,10 @@ const cursos = [
 
 /*rotas.get('/',(req,res)=> res.json({"ola":"seja bem vindo ao cfb cursos"}));*/
 
-rotas.get("/:cursoid",(req,res)=>{
-    let parametro = req.params.cursoid;
+rotas.get('/',(req,res)=>{
+    let parametro = url.parse(req.url, true).query;
     console.log(parametro);
-    let curso = cursos.find(i => i.curso == parametro);
+    let curso = cursos.find(i => i.curso.toString() == parametro.curso);
 
     if(!curso) {
         res.status(404).json({erro: "curso não encontrado",procurou : parametro});
