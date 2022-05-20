4ad62e67484f96179706f456e2b768d9bf1b0f86
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-05-20 09:47:50 -0300

    Update scraping.js

--- playground/web_scarping/scraping.js
+++ playground/web_scarping/scraping.js
@@ -1,4 +1,5 @@
 //const request = require("request");
+const { map } = require("cheerio/lib/api/traversing");
 const express = require("express");
 const puppeteer = require("puppeteer");
 const app = express();
@@ -15,24 +16,42 @@ async function obter () {
     //await page.goto('https://mundomangakun.com.br/');
    //await page.screenshot({path: 'example6.png'})
    const elemento = await page.evaluate(()=>{
-       let nome = document.querySelectorAll(".titulo-cap").innerHTML;
-        return nome;
+       let nome = document.querySelectorAll(".titulo-cap");
+
+       let rr = [];
+       let nn = Object.keys(nome);
+       for (let i= 0; i< nn.length; ++i) {
+         rr.push(nome[nn[i]].innerHTML);
+       }
+
+        return rr;
        /*let c = document.querySelectorAll(".post-projeto-background");
        return getComputedStyle(c['0']).backgroundImage;*/
    }).catch(e => console.log(e));
 
+   
    /*var css = Object.keys(elemento).reduce(
     (obj, prop) => (prop.match(/\D/) && (obj[prop] = styles[prop]), obj), {}
   );*/
+
+  let fatia = elemento.map(item => item.split("\t\t\t\t\t\t\t\t"));
+  let small = fatia.map( itens => itens[2].split("<small>"));
+  let smallDone = small.map( itens => itens[1].split(" |"));
+
+  delete small;
+  let resultado = [];
+   fatia.forEach(element => {
+    
+  });
 console.log(`elemento : `,elemento);
-console.log(elemento);
-        let nomes = [];
-        for (let i= 0; i< elemento.length; ++i) {
-          nomes.push(elemento[`${i}`]);
-          console.log(elemento[`${i}`]);
-        }
+//console.log(elemento);
+        /*let nomes = [];
+        let nn = Object.keys(elemento);
+        for (let i= 0; i< nn.length; ++i) {
+          nomes.push(elemento[nn[i]].innerHTML);
+        }*/
     await browser.close();
-    console.log(nomes);
+    console.log(smallDone);
 
 }
 
