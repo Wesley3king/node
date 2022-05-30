ebc99a93fd19848133e02d6c3c77ad1301781512
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-05-30 10:47:21 -0300

    cors

--- playground/f.js
+++ playground/f.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.post('/tst',(req, res)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
