c325148784d0c8b249af8fa4b7f65528572f11d0
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-07-03 17:43:35 -0300

    testando as imagens

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -2,22 +2,39 @@ const mongoDB = require("mongodb").MongoClient;
 const url = "mongodb+srv://king_of_shadows:fC2amEpt68SwKJCc@cluster0.mj7rklk.mongodb.net/?retryWrites=true&w=majority";
 
 //realizando a conexao com o cluster
-mongoDB.connect(url,(err,banco) =>{
+mongoDB.connect(url,async (err,banco) =>{
      if (err) console.log(err);
      const dbo = banco.db("cfbcursos");
-     const obj = [
-        {curso:"curso de lua", canal: "CFB Cursos"},
-        {curso:"curso de redux", canal: "washington"},
-        {curso:"curso de dart", canal: "washington"}
+     const col1 = [
+        {idCurso: 0, aulas: 4},
+        {idCurso: 1, aulas: 16},
+        {idCurso: 2, aulas: 63},
+        {idCurso: 3, aulas: 145},
+        {idCurso: 4, aulas: 42},
+        {idCurso: 5, aulas: 50}
+     ]
+     const col2 = [
+        {"id": 0,curso:"curso de lua"},
+        {"id": 1,curso:"curso de redux"},
+        {"id": 2,curso:"curso de dart"},
+        {"id": 3,curso:"curso de c++"},
+        {"id": 4,curso:"curso de react"},
+        {"id": 5,curso:"curso de flutter"}
     ];
      const colecao = "cursos";
-    
+     const cole2 = "join", cole3 = "join2";
+  /*   
      //inserindo dados
-   /*  dbo.collection(colecao).insertMany(obj,(e,res)=>{
+    dbo.collection(cole3).insertMany(col1,(e,res)=>{
          if (e) console.log(e);
          console.log(res.insertedCount + " cursos adicionado a db");
          //banco.close();
      });
+     dbo.collection(cole2).insertMany(col2,(e,res)=>{
+        if (e) console.log(e);
+        console.log(res.insertedCount + " cursos adicionado a db");.find()
+        //banco.close();
+    });
 
      //findOne(), find() (procurando dados)
      dbo.collection(colecao).findOne({/* filtros *//*},(e, res)=>{
@@ -54,14 +71,39 @@ mongoDB.connect(url,(err,banco) =>{
     else console.log(`${result.modifiedCount} documentos atualizados com sucesso!  `);
 
    });
+
    //updateMany
    let query = {"curso": "curso de lua"};
    let update = {$set : {"curso": "curso de PHP"}};
    dbo.collection(colecao).updateMany(query, update, (e, result) => {
     if (e) cpnsole.log(e)
     else console.log(`${result.modifiedCount} cursos atualizados`)
-   });*/
+   });
+
+   //limit (find com um numero maximo de resultados)
+   let lim = await dbo.collection(colecao).find({}).limit(2).toArray();
+   console.log(lim);*/
+
+   //join (aggregate) 
+
+   dbo.collection(cole2).aggregate([
+    {   $lookup: {
+        from: cole3,
+        localField: 'id',
+        foreignField: 'idCurso',
+        as: "detalhes"
+    }
+
+    }
+   ]).toArray((e, result)=>{
+    if (e) console.log(e);
+    console.log(JSON.stringify(result));
+   // banco.close();
+   });
+
+
    //lista todos os dados:
+   //console.log("---------------------------");
    dbo.collection(colecao).find().toArray((e, result)=>{
     if (e) console.log(e);
     console.log(result);
