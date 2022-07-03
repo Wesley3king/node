a912e21a1fb25da574b0a2d0cf0078d1adbd8239
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-07-03 15:36:31 -0300

    Update index.js

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -5,16 +5,20 @@ const url = "mongodb+srv://king_of_shadows:fC2amEpt68SwKJCc@cluster0.mj7rklk.mon
 mongoDB.connect(url,(err,banco) =>{
      if (err) console.log(err);
      const dbo = banco.db("cfbcursos");
-     const obj = {curso:"curso de lua", canal: "CFB Cursos"};
+     const obj = [
+        {curso:"curso de lua", canal: "CFB Cursos"},
+        {curso:"curso de redux", canal: "washington"},
+        {curso:"curso de dart", canal: "washington"}
+    ];
      const colecao = "cursos";
     
      //inserindo dados
-     dbo.collection(colecao).insertOne(obj,(e,res)=>{
+   /*  dbo.collection(colecao).insertMany(obj,(e,res)=>{
          if (e) console.log(e);
-         console.log("1 novo curso adicionado a db");
+         console.log(res.insertedCount + " cursos adicionado a db");
          //banco.close();
      });
-/*
+
      //findOne(), find() (procurando dados)
      dbo.collection(colecao).findOne({/* filtros *//*},(e, res)=>{
          if (e) console.log(e);
@@ -39,10 +43,24 @@ mongoDB.connect(url,(err,banco) =>{
    const deletar = {curso: /.S/};
    dbo.collection(colecao).deleteMany(deletar, (e, result)=>{
        if (e) console.log(e);
-       console.log("deletado");
+       console.log(result.deletedCount + " deletados");
    }); 
-   */
 
+   //updateOne
+   let query = {"curso": "curso de graphQL"};
+   let update = {$set : {"curso": "curso de flexbox"}};
+   dbo.collection(colecao).updateOne(query, update, (e, result)=> {
+    if (e) console.log(e)
+    else console.log(`${result.modifiedCount} documentos atualizados com sucesso!  `);
+
+   });
+   //updateMany
+   let query = {"curso": "curso de lua"};
+   let update = {$set : {"curso": "curso de PHP"}};
+   dbo.collection(colecao).updateMany(query, update, (e, result) => {
+    if (e) cpnsole.log(e)
+    else console.log(`${result.modifiedCount} cursos atualizados`)
+   });*/
    //lista todos os dados:
    dbo.collection(colecao).find().toArray((e, result)=>{
     if (e) console.log(e);
