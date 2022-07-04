f9c0e5b444495d6d14f422e89a1f8bf8f16d21de
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-07-04 11:23:17 -0300

    iniciando com mysql.
    concluidas:
    nodemon,
    senFile.

---
+++ curso de node/mysql/index.js
@@ -0,0 +1,7 @@
+const app = require("express")();
+
+app.get("/", (req, res)=> {
+
+});
+
+app.listen(5000, ()=> console.log("rodando na porta 8000"));
--- curso de node/parametros/index.js
+++ curso de node/parametros/index.js
@@ -1,6 +1,8 @@
 const app  = require("express")();
 //const app = express();
-
+app.get("/", (req, res)=>{
+	res.sendFile(`${__dirname}/index.html`)
+})
 app.get("/bemvindo/:nome", (req, res)=>{
 	res.send(`ola ${req.params.nome}, seja bem vindo`);
 });
@@ -10,4 +12,4 @@ app.get("/bemvindo/:trabalho/:nome", (req, res)=>{
 	res.send(`ola ${req.params.trabalho} ${req.params.nome}, seja bem vindo`);
 });
 
-app.listen(8000, ()=> console.log("servidor rondando na porta 80!"));
+app.listen(8000, ()=> console.log("servidor rondando na porta 8000!"));
