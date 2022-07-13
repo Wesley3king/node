409ea197089374c3b1068bf63c9a35244d1766cc
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-07-12 21:37:15 -0300

    Update index.js

--- playground/index.js
+++ playground/index.js
@@ -4,6 +4,15 @@ const rotas = require("./Routes");
 
 app.use(rotas);
 
+const things = ["contexto", "cova"];
+const regex = "co";
+const txt = `${regex}`;
+const rr = new RegExp(txt,'g')
+
+for(e of things) {
+    console.log(e.match(rr))
+}
+
 
 app.listen(process.env.PORT || 5000,()=> console.log(`servidor rodando na porta : ${process.env.PORT || 5000}`));
 
